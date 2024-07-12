a63e4d139a043db0d421eb036b89498434a943b1
Vyelen <154055300+ItsPrussia@users.noreply.github.com>
2024-07-12 14:04:16 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
   <body>
     <h1>Welcome !</h1>
     <p>this site is test site from <a href="https://github.com/ItsPrussia">ItsPrussia</a></p>
+    <?php echo "hello, this is from php"; ?>
   </body>
 </html>
