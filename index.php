c7e5d33a3cefc37dae759b56a69597f5d6ae3bb6
Vyelen <154055300+ItsPrussia@users.noreply.github.com>
2024-07-12 14:09:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     <title>Test</title>
   </head>
   <body>
-    <h1>Welcome !</h1>
-    <p>this site is test site from <a href="https://github.com/ItsPrussia">ItsPrussia</a></p>
     <?php echo "hello, this is from php"; ?>
   </body>
 </html>
